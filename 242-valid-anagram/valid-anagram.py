60844f49db067d3a771abdbeefc14792c08eb838
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2023-12-16 18:21:19 +0530

    Time: 41 ms (30.81%) | Memory: 15.3 MB (11.39%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -8,9 +8,18 @@ class Solution(object):
         if len(s)!=len(t):
             return False
 
-        
-        for ch in s:
-            if s.count(ch)!=t.count(ch):
-                return False
-        
-        return True
+        d1={}
+        d2={}
+
+        for i in range(len(s)):
+            if d1.get(s[i]):
+                d1[s[i]]+=1
+            else:
+                d1[s[i]]=1
+
+            if d2.get(t[i]):
+                d2[t[i]]+=1
+            else:
+                d2[t[i]]=1
+
+        return d1==d2
