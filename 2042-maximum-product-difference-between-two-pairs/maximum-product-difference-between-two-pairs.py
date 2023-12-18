5c86ce26f2c5f6578d6d58cc9c97d4dc20160681
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2023-12-18 10:22:24 +0530

    Time: 121 ms (83.81%) | Memory: 14.3 MB (40.08%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
@@ -5,5 +5,25 @@ class Solution(object):
         :rtype: int
         """
 
-        nums.sort()
-        return (nums[-1]*nums[-2])-(nums[0]*nums[1])
+        max1=float('-inf')
+        max2=float('-inf')
+        min1=float('inf')
+        min2=float('inf')
+
+        for i in range(len(nums)):
+
+            if nums[i] > max1:
+                max2=max1
+                max1=nums[i]
+
+            elif nums[i] > max2:
+                max2=nums[i]
+
+            if nums[i] < min1:
+                min2=min1
+                min1=nums[i]
+
+            elif nums[i]<min2:
+                min2=nums[i]
+
+        return (max1*max2)-(min1*min2)
