3e3ff71e785f12a0e2da33313e409ad649873265
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2023-12-23 19:45:38 +0530

    Time: 21 ms (23.01%) | Memory: 13.4 MB (76.11%) - LeetSync

---
+++ 1619-path-crossing/path-crossing.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def isPathCrossing(self, path):
+        """
+        :type path: str
+        :rtype: bool
+        """
+        
+        x=0
+        y=0
+
+        dic={(0,0):1}
+        
+        for i in range(len(path)):
+
+            if path[i]=="N":
+                y+=1                
+                
+            
+            elif path[i]=="S":
+                y-=1
+                
+
+            elif path[i]=="E":
+                x+=1
+                
+            
+            elif path[i]=="W":
+                x-=1
+
+            
+                
+            if (x,y) in dic.keys():
+                    return True
+                
+            dic[(x,y)]=1
+        
+        
+
+        return False
