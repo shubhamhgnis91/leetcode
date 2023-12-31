00a0438b6a6f5175f7cbb840905a804000dfb7d8
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2023-12-31 17:18:34 +0530

    Time: 20 ms (38.18%) | Memory: 13.3 MB (89.09%) - LeetSync

---
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxLengthBetweenEqualCharacters(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        dic={}
+        mlen=-1
+
+        for i in range(len(s)):
+            if s[i] not in dic:
+                dic[s[i]]=(i,i)
+            else:
+                first,_=dic[s[i]]
+                last=i
+                
+                if last-first-1 > mlen:
+                    mlen=last-first-1
+                    dic[s[i]]=(first,last)
+
+        return mlen
