21c54413332bbd14594c8877fd43e009a1113659
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2024-01-04 16:39:50 +0530

    Time: 12 ms (71.86%) | Memory: 20.9 MB (15.66%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -4,7 +4,10 @@ public:
         for(int i = 0; i < flowerbed.size() && n > 0 ; i++)
         {
             if( flowerbed[i] == 1 )
+            {
+                i++;
                 continue;
+            }
 
             bool left_empty = ( i==0 || flowerbed[i-1]==0);
             bool right_empty = (i==flowerbed.size() - 1 || flowerbed[i+1]==0);
@@ -12,7 +15,7 @@ public:
             if(left_empty && right_empty)
             {
                 n--;
-                flowerbed[i]=1;
+                flowerbed[i++]=1;
             } 
         }
 
