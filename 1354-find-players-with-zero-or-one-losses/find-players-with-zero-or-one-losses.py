751cc21226a7c2945594a9f3df609f0db206fe77
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2024-01-15 20:17:31 +0530

    Time: 1435 ms (39.69%) | Memory: 72 MB (89.11%) - LeetSync

---
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def findWinners(self, matches):
+        """
+        :type matches: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        
+        winner = {}
+        looser = {}
+        res=[]
+
+        for i in range(len(matches)):
+            
+            if matches[i][0] in winner : 
+                winner[matches[i][0]] += 1
+            
+            else:
+                winner[matches[i][0]] = 1
+
+            
+            if matches[i][1] in looser :
+                looser[matches[i][1]] += 1
+
+            else:
+                looser[matches[i][1]] = 1
+
+        row = []
+        for player in winner : 
+            if player not in looser :
+                row.append(player)
+
+        row.sort()
+        res.append(row)
+
+        row=[]
+        for player in looser : 
+            if looser[player] == 1:
+                row.append(player)
+
+        row.sort()
+        res.append(row)
+
+        return res
+
