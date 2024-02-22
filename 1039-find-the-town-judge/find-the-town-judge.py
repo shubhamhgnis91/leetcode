90b4c745c5798be5a87f19dda887f4482fd9c6ca
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2024-02-22 20:13:13 +0530

    Time: 577 ms (31.07%) | Memory: 16.8 MB (65.36%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def findJudge(self, n, trust):
+        """
+        :type n: int
+        :type trust: List[List[int]]
+        :rtype: int
+        """
+        
+        if len(trust) < 1:
+            if n == 1:
+                return 1
+            return -1
+
+        trustFreq = {}
+        trustSomeone = {}
+
+        for i in range(len(trust)):
+            trustFreq[trust[i][1]] = trustFreq.get(trust[i][1], 0) + 1
+        
+        for i in range(len(trust)):
+            trustSomeone[trust[i][0]] = True
+
+        maxTrusted = max(trustFreq.items(), key = lambda x: x[1])
+        
+        if maxTrusted[0] not in trustSomeone and maxTrusted[1] == n - 1:
+            return maxTrusted[0]
+
+        return -1 
