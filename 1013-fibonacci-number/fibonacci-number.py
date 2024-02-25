86f0633d9504ea7f349f43a50ee95f37fb70384d
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2024-02-25 20:12:24 +0530

    Time: 6 ms (98.21%) | Memory: 11.8 MB (44.82%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -5,18 +5,15 @@ class Solution(object):
         :rtype: int
         """
 
-        def fibo(n, dp):
+        if n == 0:
+            return 0
 
-            if n <= 1:
-                return n
-
-            if dp[n] != -1:
-                return dp[n]
-
-            dp[n] = fibo(n-1, dp) + fibo(n-2, dp)
-
-            return dp[n]
-        
         dp = [-1] * (n + 1)
 
-        return fibo(n, dp)
+        dp[0] = 0
+        dp[1] = 1
+
+        for i in range(2,n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
