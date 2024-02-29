38f8b2c56406123721afb30703d1bb3db6cf8f21
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2024-02-29 10:59:57 +0530

    Time: 623 ms (18.60%) | Memory: 65.8 MB (97.67%) - LeetSync

---
+++ 1731-even-odd-tree/even-odd-tree.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isEvenOddTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        stack = [root]
+        level = 0
+
+        while stack:
+            prev = None
+
+            for _ in range(len(stack)):
+
+                root = stack.pop(0)
+                print(root.val)
+
+                if level % 2 == 0:
+                    if root.val % 2 == 0:
+                        return False
+
+                    if prev is not None and prev.val >= root.val:
+                        return False
+                
+                else:
+                    if root.val % 2 != 0:
+                        return False
+
+                    if prev is not None and prev.val <= root.val:
+                        return False
+
+
+                if root.left:
+                    stack.append(root.left)                
+
+                if root.right:
+                    stack.append(root.right)
+
+                prev = root
+
+            level += 1
+
+        return True
