5b8183e6be3590815103104bf198c580913545f4
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2024-03-01 10:10:02 +0530

    Time: 19 ms (52.21%) | Memory: 11.7 MB (72.06%) - LeetSync

---
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def maximumOddBinaryNumber(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+        res = ""
+        bitFreq = {}
+
+        for bit in s:
+            bitFreq[bit] = bitFreq.get(bit, 0) + 1
+        
+        if '0' not in bitFreq:
+            return s
+
+        oneBits = bitFreq['1']
+        zeroBits = bitFreq['0']
+
+        print(oneBits)
+        print(zeroBits)
+
+        while oneBits > 1:
+            res += '1'
+            oneBits -= 1
+        
+        while zeroBits:
+            res += '0'
+            zeroBits -= 1
+
+        res += '1'
+
+        return res
+
