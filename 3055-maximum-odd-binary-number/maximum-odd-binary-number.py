129e26aa2d0905fe6f7ea7a1616e6619e5539327
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2024-03-01 10:14:29 +0530

    Time: 17 ms (62.50%) | Memory: 11.7 MB (44.12%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
@@ -5,17 +5,10 @@ class Solution(object):
         :rtype: str
         """
         
-        res = ""
-        bitFreq = {}
+        res = ""        
 
-        for bit in s:
-            bitFreq[bit] = bitFreq.get(bit, 0) + 1
-        
-        if '0' not in bitFreq:
-            return s
-
-        oneBits = bitFreq['1']
-        zeroBits = bitFreq['0']
+        oneBits = s.count('1')
+        zeroBits = len(s) - oneBits
 
         res += '1' * (oneBits - 1)
         res += '0' * (zeroBits)
