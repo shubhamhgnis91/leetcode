3d38e4c40fef50cca136420066ab1bf4da62adb6
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2024-03-09 20:19:27 +0530

    Time: 32 ms (46.27%) | Memory: 11.9 MB (25.73%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        
+        res = []
+        freq1 = {}
+        freq2 = {}
+
+        for num in nums1:
+            freq1[num] = freq1.get(num, 0) + 1        
+        
+        for num in nums2:
+            freq2[num] = freq2.get(num, 0) + 1
+
+        
+        if len(freq1) < len(freq2):
+            freq1, freq2 = freq2, freq1
+
+        for num in freq1:
+            count = 0
+            if num in freq2:
+                count = min(freq1[num], freq2[num])
+
+                while count > 0:
+                    res.append(num)
+                    count -= 1
+        
+        return res
+
+ 
+        
