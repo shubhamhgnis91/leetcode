87eaaac3526860b34300428bddd58eca0f4760d1
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2024-03-09 20:23:33 +0530

    Time: 20 ms (95.52%) | Memory: 11.7 MB (90.83%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -8,28 +8,15 @@ class Solution(object):
         
         res = []
         freq1 = {}
-        freq2 = {}
+        
 
         for num in nums1:
             freq1[num] = freq1.get(num, 0) + 1        
         
         for num in nums2:
-            freq2[num] = freq2.get(num, 0) + 1
+            if num in freq1 and freq1[num] > 0:
+                res.append(num)
+                freq1[num] -= 1
 
-        
-        if len(freq1) > len(freq2):
-            freq1, freq2 = freq2, freq1
-
-        for num in freq1:
-            count = 0
-            if num in freq2:
-                count = min(freq1[num], freq2[num])
-
-                while count > 0:
-                    res.append(num)
-                    count -= 1
-        
         return res
-
- 
         
