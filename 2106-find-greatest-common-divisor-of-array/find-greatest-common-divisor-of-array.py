53a292607723d22231032bda869c99031652707a
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2024-03-17 15:38:09 +0530

    Time: 41 ms (16.25%) | Memory: 11.6 MB (98.90%) - LeetSync

---
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findGCD(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        divisor = min(nums)
+        dividend = max(nums)
+
+        while True:
+
+            quotient = dividend // divisor
+            remainder = dividend % divisor
+
+            if remainder == 0:
+                return divisor
+
+            dividend = divisor
+            divisor = remainder 
