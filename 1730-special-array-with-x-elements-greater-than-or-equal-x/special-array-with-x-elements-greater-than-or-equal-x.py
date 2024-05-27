1268b9a3031be92a71e4a0e104248839a5346cfd
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2024-05-27 14:12:30 +0530

    Time: 39 ms (67.83%) | Memory: 16.4 MB (86.58%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,6 +1,7 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
         
+        nums.sort()
 
         def count(n: int, nums: List[int]) -> int:
             
@@ -12,10 +13,21 @@ class Solution:
             return ans
 
 
-        for x in range(len(nums) + 1):
-            ans = count(x, nums)
+        l = 0
+        r = len(nums)
 
-            if x == ans:
-                return ans
+        while l <= r:
+            m = (l + r) // 2
+
+            ans = count(m, nums)
+
+            if m == ans:
+                return m
+
+            if ans > m:
+                l = m + 1
+
+            else:
+                r = m - 1
 
         return -1
