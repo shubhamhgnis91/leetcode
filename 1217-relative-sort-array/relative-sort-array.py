653a8184b28754d51e5c5f455c353e4ee4008ba0
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2024-06-11 19:39:28 +0530

    Time: 34 ms (88.88%) | Memory: 16.6 MB (93.48%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,23 +1,21 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         
-        d = {}
+        d = [0] * 1001
         res = []
-        temp = []
-
 
         for n in arr1:
-            d[n] = d.get(n, 0) + 1
+            d[n] += 1
 
         for i in range(len(arr2)):
             while d[arr2[i]] > 0:
                 res.append(arr2[i])
                 d[arr2[i]] -= 1
 
-        for n in d:
-            while d[n] > 0:
-                temp.append(n)
-                d[n] -= 1
+        for i in range(len(d)):
+            while d[i] > 0:
+                res.append(i)
+                d[i] -= 1
 
-        return res + sorted(temp)
+        return res
         
