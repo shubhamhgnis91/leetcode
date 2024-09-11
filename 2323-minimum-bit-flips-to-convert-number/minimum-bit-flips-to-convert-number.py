4b6c624ef07be55b7f4dd1cbc38d359e5524363a
Shubham Singh <65723542+shubhamhgnis91@users.noreply.github.com>
2024-09-11 18:44:00 +0530

    Time: 27 ms (93.90%) | Memory: 16.5 MB (26.96%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -1,6 +1,4 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
         
-        res = start ^ goal
-
-        return bin(res)[2:].count('1')
+        return bin(start ^ goal)[2:].count('1')
